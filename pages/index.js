1d0472dca70b096aad9b22adcafb3e527b673733
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-03-15 17:05:11 +0100

    Display fixed

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { getFeaturedEvents } from '../dummy-data';
-
+import EventList from '../components/events/event-list';
 
 function RootPage() {
     const featuredEvents = getFeaturedEvents();
 
     return (
         <div>
-            <h1>Home</h1>
+            <EventList items={featuredEvents}/>
         </div>
     );
 }
