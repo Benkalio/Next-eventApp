4d1e1aa1ca89bf3397480c9eced72af5049fc3a4
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-03-18 23:49:47 +0100

    Navigation bar added to traverse through pages

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,5 +1,3 @@
-
-
 import Link from 'next/link';
 
 import classes from './button.module.css';
--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,9 +1,34 @@
+import { useState, Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 function EventDetailPage() {
+    const router = useRouter()
+    const [backgroundColor] = useState('green')
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return <p style={{'backgroundColor': backgroundColor}}>No event found!</p>
+    }
+
     return (
-        <div>
-            <h1>Details of Events</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
 }
 
