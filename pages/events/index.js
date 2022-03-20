5aea1db2b0e62b6f57ac4513e836c0ffdb89e731
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-03-20 21:00:39 +0100

    Search for month and year added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,23 @@
 import { Fragment } from "react";
+import { useRouter } from 'next/router';
 
 import { getAllEvents } from "../../dummy-data";
 import EventSearch from '../../components/events/event-search'
 import EventList from "../../components/events/event-list";
 
 function EventPage() {
+    const router = useRouter();
     const events = getAllEvents();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        
+        router.push(fullPath);
+    }
 
     return (
         <Fragment>
-            <EventSearch />
+            <EventSearch onSearch={findEventsHandler} />
             <EventList items={events}/>
         </Fragment>
     );
